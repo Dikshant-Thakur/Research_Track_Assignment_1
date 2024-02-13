d5f3b85f15e3fd327b2931161acd91186920a5dd
Dikshant Thakur <56224054+Dikshant-Thakur@users.noreply.github.com>
2024-02-13 15:19:25 +0100

    UPDATED

--- assignement1.py
+++ assignement1.py
@@ -3,33 +3,6 @@ from __future__ import print_function
 import time
 from sr.robot import *
 
-"""
-Exercise 3 python script
-
-We start from the solution of the exercise 2
-Put the main code after the definition of the functions. The code should make the robot:
-	- 1) find and grab the closest silver marker (token)
-	- 2) move the marker on the right
-	- 3) find and grab the closest golden marker (token)
-	- 4) move the marker on the right
-	- 5) start again from 1
-
-The method see() of the class Robot returns an object whose attribute info.marker_type may be MARKER_TOKEN_GOLD or MARKER_TOKEN_SILVER,
-depending of the type of marker (golden or silver). 
-Modify the code of the exercise2 to make the robot:
-
-1- retrieve the distance and the angle of the closest silver marker. If no silver marker is detected, the robot should rotate in order to find a marker.
-2- drive the robot towards the marker and grab it
-3- move the marker forward and on the right (when done, you can use the method release() of the class Robot in order to release the marker)
-4- retrieve the distance and the angle of the closest golden marker. If no golden marker is detected, the robot should rotate in order to find a marker.
-5- drive the robot towards the marker and grab it
-6- move the marker forward and on the right (when done, you can use the method release() of the class Robot in order to release the marker)
-7- start again from 1
-
-	When done, run with:
-	$ python run.py exercise3.py
-
-"""
 
 
 a_th = 2.0
